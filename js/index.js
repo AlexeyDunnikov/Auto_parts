2cb0778ed444d026ab4d6e3f1245012db6159e97
”Dunnikov <dynnikovleha@gmail.com>
2022-01-03 21:20:17 +0300

    save

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-import { Producer } from "./classes/producer.js";
-import { Detail } from "./classes/detail.js";
-import { Order } from "./classes/order.js";
-import { state } from "./state.js";
 import { initialNavbar } from "./navbar.js";
 
 const navbar = document.querySelector(".tabs");
@@ -9,21 +5,3 @@ const instance = M.Tabs.init(navbar, {});
 
 initialNavbar();
 
-const addForm = document.querySelector(".add__form");
-addForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  const target = evt.target;
-
-  const producerName = target.producer.value;
-  const producerAddress = target.address.value;
-  const producerPhone = target.phone.value;
-
-  const producer = new Producer(producerName, producerAddress, producerPhone);
-  const obj = {
-    producer,
-  };
-
-  state.push(obj);
-
-  console.log(state);
-});
